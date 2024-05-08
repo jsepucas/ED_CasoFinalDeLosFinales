8eba5bd415b6fed4c90b96aa826050a91104b985
jsepucas <jsepucas@myuax.com>
2024-05-08 09:58:12 +0200

    Commitss

--- src/main/java/C_GUI/VentanaPrincipal.java
+++ src/main/java/C_GUI/VentanaPrincipal.java
@@ -12,3 +12,20 @@ import java.util.List;
 public class VentanaPrincipal extends JFrame {
     private CuentaUsuario usuarioActual;
     private List<CuentaUsuario> todosLosUsuarios;
+
+    private JTextArea textAreaInfo;
+    private JTextField textFieldAlias, textFieldCorreo, textFieldMensaje;
+    private JButton btnCrearUsuario, btnSeguirUsuario, btnPublicarTuit;
+
+    public VentanaPrincipal() {
+        super("Simulación de Twitter");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLayout(new BorderLayout());
+        setLocationRelativeTo(null);  // Centrar la ventana en la pantalla
+
+        todosLosUsuarios = new ArrayList<>();
+        initUIComponents();
+
+        
+    }
