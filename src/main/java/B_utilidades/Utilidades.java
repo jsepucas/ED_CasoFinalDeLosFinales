79fcf9e077af431f8733bb7ad7fe645c91f46e73
jsepucas <jsepucas@myuax.com>
2024-05-08 10:20:47 +0200

    Commitss

--- src/main/java/B_utilidades/Utilidades.java
+++ src/main/java/B_utilidades/Utilidades.java
@@ -1,5 +1,9 @@
 package B_utilidades;
 
+import A_modelo.CuentaUsuario;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Utilidades {
     public static boolean esCorreoValido(String correo) {
@@ -7,6 +11,14 @@ public class Utilidades {
     }
 
     public static boolean esAliasValido(String alias) {
-        return alias != null && alias.matches("\\w+");
+        return alias != null && alias.matches("[a-zA-Z0-9]+");
+    }
+
+    public static void ordenarPorCorreo(List<CuentaUsuario> usuarios) {
+        Collections.sort(usuarios, Comparator.comparing(CuentaUsuario::getEmail));
+    }
+
+    public static void ordenarPorAlias(List<CuentaUsuario> usuarios) {
+        Collections.sort(usuarios, Comparator.comparing(CuentaUsuario::getAlias));
     }
 }
