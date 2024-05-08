c0fd365fd187fce7fe526460e3f46b6c7b4e0bc7
jsepucas <jsepucas@myuax.com>
2024-05-08 10:51:00 +0200

    Commitss

---
+++ src/main/java/D_Ejercicio3/CodigoCorregido.java
@@ -0,0 +1,20 @@
+package D_Ejercicio3;
+
+public class CodigoCorregido {
+    public static void main(String[] args) {
+        int a = recursive(1, -2);
+        System.out.println(a);
+    }
+
+    public static int recursive(int a, int b) {
+        if (b < 0) {
+            return 0; // Manejo de casos donde b es negativo.
+        } else if (b == 0) {
+            return 1;
+        } else if (a == 0) {
+            return 0;
+        } else {
+            return a * recursive(a, b - 1);
+        }
+    }
+}
