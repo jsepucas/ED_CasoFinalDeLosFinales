e034d698bb10b4adcdf51845c789239b231f549a
jsepucas <jsepucas@myuax.com>
2024-05-08 10:38:24 +0200

    Commitss

--- src/main/java/A_modelo/Retuit.java
+++ src/main/java/A_modelo/Retuit.java
@@ -4,7 +4,7 @@ public class Retuit extends Tuit {
     private Tuit tuitOriginal;
 
     public Retuit(Tuit tuitOriginal, CuentaUsuario emisor, long momento) {
-        super(tuitOriginal.message, emisor, momento);
+        super(tuitOriginal.message, emisor);
         this.tuitOriginal = tuitOriginal;
     }
 }
