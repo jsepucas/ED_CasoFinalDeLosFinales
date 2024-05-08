6278bcc6bbdf25e8f99a274fc7f5f2e60ce12130
jsepucas <jsepucas@myuax.com>
2024-05-08 09:53:25 +0200

    Commitss

--- src/main/java/C_GUI/VentanaPrincipal.java
+++ src/main/java/C_GUI/VentanaPrincipal.java
@@ -1,4 +1,18 @@
 package C_GUI;
 
-public class VentanaPrincipal {
-}
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class VentanaPrincipal extends JFrame {
+    public VentanaPrincipal() {
+        super("Simulación de Twitter");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setLayout(new BorderLayout());
+        setLocationRelativeTo(null);  // Pantalla en centro
+
+        // Puedo añadir mas componentes
+
+        initUIComponents();
