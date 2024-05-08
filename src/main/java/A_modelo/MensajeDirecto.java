8ac4535a94f52383d76f183383f5d1b7188bdb74
jsepucas <jsepucas@myuax.com>
2024-05-08 10:38:14 +0200

    Commitss

--- src/main/java/A_modelo/MensajeDirecto.java
+++ src/main/java/A_modelo/MensajeDirecto.java
@@ -4,7 +4,7 @@ public class MensajeDirecto extends Tuit {
     private CuentaUsuario receptor;
 
     public MensajeDirecto(String mensaje, CuentaUsuario emisor, CuentaUsuario receptor, long momento) {
-        super(mensaje, emisor, momento);
+        super(mensaje, emisor);
         this.receptor = receptor;
     }
 }
