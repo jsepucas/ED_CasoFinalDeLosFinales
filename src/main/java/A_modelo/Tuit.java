7b14dac59f66eb4aa030bcae2480b378885c6e62
jsepucas <jsepucas@myuax.com>
2024-05-08 10:20:43 +0200

    Commitss

--- src/main/java/A_modelo/Tuit.java
+++ src/main/java/A_modelo/Tuit.java
@@ -1,13 +1,28 @@
 package A_modelo;
 
-public class Tuit {
-    protected String mensaje;
-    protected CuentaUsuario emisor;
-    protected long momento;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
-    public Tuit(String mensaje, CuentaUsuario emisor, long momento) {
-        this.mensaje = mensaje;
-        this.emisor = emisor;
-        this.momento = momento;
+public class Tuit {
+    protected String message;
+    protected CuentaUsuario sender;
+    protected LocalDate time;
+
+    public Tuit(String message, CuentaUsuario sender, long momento) {
+        if (message.length() > 140)
+            throw new IllegalArgumentException("Message exceeds 140 characters");
+        this.message = message;
+        this.sender = sender;
+        this.time = Instant.ofEpochMilli(momento).atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    @Override
+    public String toString() {
+        return "Tweet{" +
+                "message='" + message + '\'' +
+                ", sender=" + sender.getAlias() +
+                ", time=" + time +
+                '}';
     }
 }
