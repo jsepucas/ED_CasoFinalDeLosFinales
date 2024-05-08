65fa33b1c97a1c3ea16e5d2975d916ae4a8389b4
jsepucas <jsepucas@myuax.com>
2024-05-08 09:46:31 +0200

    Commitss

--- src/main/java/B_utilidades/Utilidades.java
+++ src/main/java/B_utilidades/Utilidades.java
@@ -1,4 +1,12 @@
 package B_utilidades;
 
+
 public class Utilidades {
+    public static boolean esCorreoValido(String correo) {
+        return correo != null && correo.contains("@");
+    }
+
+    public static boolean esAliasValido(String alias) {
+        return alias != null && alias.matches("\\w+");
+    }
 }
