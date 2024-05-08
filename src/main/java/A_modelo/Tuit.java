30fc5dc1dfea86849b39770298f87e8ec5f6314e
jsepucas <jsepucas@myuax.com>
2024-05-08 09:47:04 +0200

    Commitss

--- src/main/java/A_modelo/Tuit.java
+++ src/main/java/A_modelo/Tuit.java
@@ -11,3 +11,21 @@ public class Tuit {
         this.momento = momento;
     }
 }
+
+public class MensajeDirecto extends Tuit {
+    private CuentaUsuario receptor;
+
+    public MensajeDirecto(String mensaje, CuentaUsuario emisor, CuentaUsuario receptor, long momento) {
+        super(mensaje, emisor, momento);
+        this.receptor = receptor;
+    }
+}
+
+public class Retuit extends Tuit {
+    private Tuit tuitOriginal;
+
+    public Retuit(Tuit tuitOriginal, CuentaUsuario emisor, long momento) {
+        super(tuitOriginal.mensaje, emisor, momento);
+        this.tuitOriginal = tuitOriginal;
+    }
+}
