b347629239aa43dba2de3b89de0b45ec9f35fa62
jsepucas <jsepucas@myuax.com>
2024-05-08 11:33:20 +0200

    Commitss

---
+++ src/main/java/F_InicioDeSesion/GestorAutenticacion.java
@@ -0,0 +1,8 @@
+package F_InicioDeSesion;
+
+public class GestorAutenticacion {
+    public static boolean autenticar(String usuario, String contraseña) {
+        // Ahora cualquier usuario y contraseña son aceptados
+        return true;
+    }
+}
