ceab7e790c63185f9aa8dc5d37c396a8f2b61576
jsepucas <jsepucas@myuax.com>
2024-05-08 09:45:41 +0200

    Commitss

--- src/main/java/A_modelo/CuentaUsuario.java
+++ src/main/java/A_modelo/CuentaUsuario.java
@@ -4,3 +4,33 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+
+public class CuentaUsuario {
+    private String alias;
+    private String correoElectronico;
+    private List<Tuit> tuits;
+    private List<Tuit> lineaDeTiempo;
+    private Set<CuentaUsuario> seguidores;
+    private Set<CuentaUsuario> siguiendo;
+
+    public CuentaUsuario(String alias, String correoElectronico) {
+        if (!Utilidades.esCorreoValido(correoElectronico)) {
+            throw new IllegalArgumentException("Dirección de correo electrónico inválida.");
+        }
+        if (!Utilidades.esAliasValido(alias)) {
+            throw new IllegalArgumentException("El alias debe contener solo letras y números.");
+        }
+        this.alias = alias;
+        this.correoElectronico = correoElectronico;
+        this.tuits = new LinkedList<>();
+        this.lineaDeTiempo = new LinkedList<>();
+        this.seguidores = new HashSet<>();
+        this.siguiendo = new HashSet<>();
+    }
+
+    public void seguir(CuentaUsuario usuario) {
+        if (usuario != null && !this.siguiendo.contains(usuario)) {
+            this.siguiendo.add(usuario);
+            usuario.seguidores.add(this);
+        }
+    }
