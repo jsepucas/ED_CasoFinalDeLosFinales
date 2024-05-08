5e168fa1e31f3e5967b3dbe61cce19f784461dd3
jsepucas <jsepucas@myuax.com>
2024-05-08 09:51:55 +0200

    Commitss

--- src/main/java/C_GUI/MainGUI.java
+++ src/main/java/C_GUI/MainGUI.java
@@ -1,4 +1,14 @@
 package C_GUI;
 
+
+import javax.swing.*;
+
 public class MainGUI {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            VentanaPrincipal ventana = new VentanaPrincipal();
+            ventana.setVisible(true);
+        });
+    }
 }
+
