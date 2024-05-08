6bbc6aa2a80df77faa8b26e6bd6bd85b35ae9aa3
jsepucas <jsepucas@myuax.com>
2024-05-08 09:46:52 +0200

    Commitss

--- src/main/java/A_modelo/Tuit.java
+++ src/main/java/A_modelo/Tuit.java
@@ -1,4 +1,13 @@
 package A_modelo;
 
 public class Tuit {
+    protected String mensaje;
+    protected CuentaUsuario emisor;
+    protected long momento;
+
+    public Tuit(String mensaje, CuentaUsuario emisor, long momento) {
+        this.mensaje = mensaje;
+        this.emisor = emisor;
+        this.momento = momento;
+    }
 }
